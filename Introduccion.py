acb8e6bdac96753993f553fe7bebd7a958f70e91
Juan Alejandro Flores Perez <JFLORES@bancomext.gob.mx>
2023-05-04 11:41:57 -0600

    Prueba vizualizar pdf

--- Introduccion.py
+++ Introduccion.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import base64
-import logging
-import tempfile
+import streamlit.components.v1 as components
 
 st.set_page_config(
     page_title="Turismo",
@@ -13,10 +12,11 @@ st.title('Tablero indicadores económicos de Turismo')
 # st.balloons()
 
 def show_pdf(file_path):
-    with open(file_path,"rb") as f:
+    with open(file_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="650" height="650" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+    # st.markdown(pdf_display, unsafe_allow_html=True)
+    components.iframe(src=f"data:application/pdf;base64,{base64_pdf}", width=800, height=800)
 
 show_pdf("panorama.pdf")
 
