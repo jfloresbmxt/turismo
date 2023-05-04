cac3663d21fb191111df68028f8ab5a1fed2d463
Juan Alejandro Flores Perez <JFLORES@bancomext.gob.mx>
2023-05-04 12:09:40 -0600

    Prueba vizualizar pdf

--- Introduccion.py
+++ Introduccion.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import base64
-import streamlit.components.v1 as components
+from  PIL import Image
 
 st.set_page_config(
     page_title="Turismo",
@@ -9,25 +9,33 @@ st.set_page_config(
 
 st.title('Tablero indicadores económicos de Turismo')
 
-# st.balloons()
+st.header("Objetivo")
+st.markdown('Objetivo del tablero')
+st.divider()
 
+st.header("Otros estudios")
 def show_pdf(file_path):
-    with open(file_path, "rb") as f:
+    with open(file_path,"rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="650" height="650" type="application/pdf"></iframe>'
-    # st.markdown(pdf_display, unsafe_allow_html=True)
-    components.iframe(src=f"data:application/pdf;base64,{base64_pdf}", width=800, height=800)
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
-show_pdf("panorama.pdf")
+feature_image1 = Image.open('bmxt.jpg')
+with st.container():
+    image_col, text_col = st.columns((1,3))
+    with image_col:
+        st.image(feature_image1)
+    with text_col:
+        st.markdown(""" <style> .font {
+            font-size:22px ; font-family: 'Black'; color: black;} 
+            </style> """, unsafe_allow_html=True)
+        st.markdown('<p class="font">Panorama económico</p>', unsafe_allow_html=True)    
+        st.markdown("Conoce el comportamiento de los indicadores económicos mas importantes del país")
 
-# with open("panorama.pdf", "rb") as pdf_file:
-#     PDFbyte = pdf_file.read()
-
-
-# st.download_button(label="Export_Report",
-#                     data=PDFbyte,
-#                     file_name="test.pdf",
-#                     mime='application/octet-stream')
-
-# with st.expander("Panorama Economico"):
-    # show_pdf('panorama.pdf')
+with st.container():
+    with open("panorama.pdf", "rb") as pdf_file:
+        PDFbyte = pdf_file.read()
+    st.download_button(label="Descargar el estudio",
+                    data=PDFbyte,
+                    file_name="panorama_economico.pdf",
+                    mime='application/octet-stream')
