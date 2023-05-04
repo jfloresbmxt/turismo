137500f8cd156a1b48b18ee85d5e1ad87e8efa05
Juan Alejandro Flores Perez <JFLORES@bancomext.gob.mx>
2023-05-04 11:11:43 -0600

    Prueba vizualizar pdf

--- Introduccion.py
+++ Introduccion.py
@@ -21,7 +21,7 @@ st.title('Tablero indicadores económicos de Turismo')
 
 with open("panorama.pdf","rb") as f:
     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></embed>'
+pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
 st.markdown(pdf_display, unsafe_allow_html=True)
 
 
