64afe5cf5f6ff04395af17fc4fda2e5899e38567
Juan Alejandro Flores Perez <JFLORES@bancomext.gob.mx>
2023-05-04 11:29:42 -0600

    Prueba vizualizar pdf

--- Introduccion.py
+++ Introduccion.py
@@ -17,7 +17,24 @@ def show_pdf(file_path):
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="650" height="650" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
+
 # show_pdf("panorama.pdf")
+
+
+uploaded = st.file_uploader(label="panorama", type="pdf")
+if uploaded is None:
+    st.stop()
+
+base64_pdf = base64.b64encode(uploaded.read()).decode("utf-8")
+pdf_display = (
+    f'<embed src="data:application/pdf;base64,{base64_pdf}" '
+    'width="800" height="1000" type="application/pdf"></embed>'
+)
+st.markdown(pdf_display, unsafe_allow_html=True)
+
+
+
+
 
 # with open("panorama.pdf", "rb") as pdf_file:
 #     PDFbyte = pdf_file.read()
