5a771279b418cd6f19c4a524dff179a785029257
Juan Alejandro Flores Perez <JFLORES@bancomext.gob.mx>
2023-05-04 10:11:27 -0600

    Agrega pdf de indicadores economicos

--- Introduccion.py
+++ Introduccion.py
@@ -16,13 +16,18 @@ st.balloons()
 #     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="650" height="650" type="application/pdf"></iframe>'
 #     st.markdown(pdf_display, unsafe_allow_html=True)
 
-with open("panorama.pdf", "rb") as pdf_file:
-    PDFbyte = pdf_file.read()
+# with open("panorama.pdf", "rb") as pdf_file:
+#     PDFbyte = pdf_file.read()
+with open("panorama.pdf","rb") as f:
+    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+st.markdown(pdf_display, unsafe_allow_html=True)
 
-st.download_button(label="Export_Report",
-                    data=PDFbyte,
-                    file_name="test.pdf",
-                    mime='application/octet-stream')
+
+# st.download_button(label="Export_Report",
+#                     data=PDFbyte,
+#                     file_name="test.pdf",
+#                     mime='application/octet-stream')
 
 # with st.expander("Panorama Economico"):
     # show_pdf('panorama.pdf')
