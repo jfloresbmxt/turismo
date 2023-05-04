ddac08bb4e172a3d9549192785b8c98acabab216
Juan Alejandro Flores Perez <JFLORES@bancomext.gob.mx>
2023-05-04 11:26:54 -0600

    Prueba vizualizar pdf

--- Introduccion.py
+++ Introduccion.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import base64
+import logging
+import tempfile
 
 st.set_page_config(
     page_title="Turismo",
@@ -10,19 +12,15 @@ st.title('Tablero indicadores económicos de Turismo')
 
 # st.balloons()
 
-# def show_pdf(file_path):
-#     with open(file_path,"rb") as f:
-#         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-#     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="650" height="650" type="application/pdf"></iframe>'
-#     st.markdown(pdf_display, unsafe_allow_html=True)
+def show_pdf(file_path):
+    with open(file_path,"rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="650" height="650" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+# show_pdf("panorama.pdf")
 
 # with open("panorama.pdf", "rb") as pdf_file:
 #     PDFbyte = pdf_file.read()
-
-with open("panorama.pdf","rb") as f:
-    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-st.markdown(pdf_display, unsafe_allow_html=True)
 
 
 # st.download_button(label="Export_Report",
