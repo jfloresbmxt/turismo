34a4f082441821911356682bb5a0927a500333ad
Juan Alejandro Flores Perez <JFLORES@bancomext.gob.mx>
2023-05-04 09:52:08 -0600

    Agrega pdf de indicadores economicos

--- Introduccion.py
+++ Introduccion.py
@@ -10,11 +10,19 @@ st.title('Tablero indicadores económicos de Turismo')
 
 st.balloons()
 
-def show_pdf(file_path):
-    with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="650" height="650" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+# def show_pdf(file_path):
+#     with open(file_path,"rb") as f:
+#         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+#     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="650" height="650" type="application/pdf"></iframe>'
+#     st.markdown(pdf_display, unsafe_allow_html=True)
 
-with st.expander("Panorama Economico"):
-    show_pdf('panorama.pdf')
+with open("panorama.pdf", "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+
+st.download_button(label="Export_Report",
+                    data=PDFbyte,
+                    file_name="test.pdf",
+                    mime='application/octet-stream')
+
+# with st.expander("Panorama Economico"):
+    # show_pdf('panorama.pdf')
